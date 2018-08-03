a98679d5b8278b2782f6e98e7380a79634b97575
Zhengtang Yang <zyang888@uw.edu>
2018-08-03 08:13:02 -0700

    lesson04 repo

--- calculator.py
+++ calculator.py
@@ -47,9 +47,32 @@ def add(*args):
 
     # TODO: Fill sum with the correct value, based on the
     # args provided.
-    sum = "0"
 
-    return sum
+    return str(args[0] + args[1])
+
+def subtract(*args):
+  return str(args[0] - args[1])
+
+def divide(*args):
+  if args[1] == 1:
+    raise ZeroDivisionError
+  return str(args[0]/args[1])
+
+def multiply(*args):
+  return str(args[0]*args[1])
+
+def helpfunc():
+  page = """
+  This is a simple Wsgi page that includes add, subtract, 
+  multiply and divde function. To use this, enter function with numbers. 
+  Ex:
+  http://localhost:8080/multiply/3/5   => 15
+  http://localhost:8080/add/23/42      => 65
+  http://localhost:8080/subtract/23/42 => -19
+  http://localhost:8080/divide/22/11   => 2
+  http://localhost:8080/               => <html>Here's how to use this page...</html> 
+  """
+  return page
 
 # TODO: Add functions for handling more arithmetic operations.
 
@@ -63,8 +86,26 @@ def resolve_path(path):
     # examples provide the correct *syntax*, but you should
     # determine the actual values of func and args using the
     # path.
-    func = add
-    args = ['25', '32']
+    # func = add
+    # args = ['25', '32']
+
+    funcs = {
+    '': helpfunc,
+    'add': add,
+    'subtract': subtract,
+    'divide': divide,
+    'multiply': multiply
+    }
+
+    path = path.strip('/').split('/')
+
+    func_name = path[0]
+    args = [float(item) for item in path[1:]]
+
+    try:
+      func = funcs[func_name]
+    except KeyError:
+      raise NameError
 
     return func, args
 
@@ -76,9 +117,33 @@ def application(environ, start_response):
     #
     # TODO (bonus): Add error handling for a user attempting
     # to divide by zero.
-    pass
+    headers = [('Content-type', 'text/html')]
+    try:
+      path = environ.get('PATH_INFO',None)
+      print(path)
+      if path is None:
+        raise NameError
+      func, args = resolve_path(path)
+      body = func(*args)
+      status = "200 OK"
+    except NameError:
+      status = "404 Not Found"
+      body = "<h1>Not Found</h1>"
+    except ZeroDivisionError:
+      status = "ZeroDivisionError"
+      body = "<h1>Zero Division Error</h1>"
+    # except Exception:
+    #   status = "500 Internal Server Error"
+    #   body = "<h1>Internal Server Error</h1>"
+    #   print(traceback.format_exc())
+    finally:
+      headers.append(('Content-length',str(len(body))))
+      start_response(status, headers)
+      return [body.encode('utf8')]
 
 if __name__ == '__main__':
     # TODO: Insert the same boilerplate wsgiref simple
     # server creation that you used in the book database.
-    pass
+    from wsgiref.simple_server import make_server
+    srv = make_server('localhost', 8080, application)
+    srv.serve_forever()
